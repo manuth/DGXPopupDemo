20f6c3c81bd02b341aedfe2f2934426cdd2702cd
Manuel Thalmann <m@nuth.ch>
2023-05-16 15:31:48 +0200

    Add data for the grid

---
+++ Customer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Net.Mail;
+
+namespace DGXPopupDemo
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public MailAddress Email { get; set; }
+        public bool IsMember { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,42 @@ namespace DGXPopupDemo
         public MainWindow()
         {
             InitializeComponent();
+
+            dataGrid.DataContext = new List<Customer>()
+            {
+                new Customer()
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = new MailAddress("john.doe@contoso.com"),
+                    IsMember = false,
+                    Status = OrderStatus.Pending
+                },
+                new Customer()
+                {
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    Email = new MailAddress("jane.doe@contoso.com"),
+                    IsMember = true,
+                    Status = OrderStatus.Paid
+                },
+                new Customer()
+                {
+                    FirstName = "Max",
+                    LastName = "Mustermann",
+                    Email = new MailAddress("max.mustermann@contoso.com"),
+                    IsMember = true,
+                    Status = OrderStatus.Shipped
+                },
+                new Customer()
+                {
+                    FirstName = "Jack",
+                    LastName = "Black",
+                    Email = new MailAddress("jack.black@contoso.com"),
+                    IsMember = true,
+                    Status = OrderStatus.Finished
+                }
+            };
         }
     }
 }
